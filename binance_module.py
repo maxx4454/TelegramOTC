de41330882043e0d95efb47956fb918338606ac9
maxx4454 <93541282+maxx4454@users.noreply.github.com>
2022-08-12 11:20:26 +0300

    Update binance_module.py

--- binance_module.py
+++ binance_module.py
@@ -3,21 +3,29 @@ import logging
 from binance.spot import Spot as Client
 from binance.lib.utils import config_logging
 
-key = 'GQHP7JzHIyCINjBDI9H9S12VYtIm5lZvhV6Bxjp0ypRnkYm073Mll88YWomTZXI4'
-secret = 'Fa5cVO90wPEiIPqhRUuWVIfVCQ4Bx23qbUr1LP7bKIzbEQQ4cnZRF9GZmCwiPAN6'
-spot_client = Client(key, secret, show_header=True)
 
+class Deposits:
+    def __init__(self):
+        self.key = 'GQHP7JzHIyCINjBDI9H9S12VYtIm5lZvhV6Bxjp0ypRnkYm073Mll88YWomTZXI4'
+        self.secret = 'Fa5cVO90wPEiIPqhRUuWVIfVCQ4Bx23qbUr1LP7bKIzbEQQ4cnZRF9GZmCwiPAN6'
+        self.address = '0xad0625b44debf3a1b76acec973fb051ee8b2fa5f'
+        self.spot_client = Client(self.key, self.secret, show_header=True)
 
-def withdraw(amount: int, address):
-    print(spot_client.withdraw(coin="BUSD", amount=amount, address=address))
+    @staticmethod
+    def withdraw(self, amount: int, address):
+        print(self.spot_client.withdraw(coin="BUSD", amount=amount, address=address))
 
+    @staticmethod
+    def check_deposit(self, amount: int, tx_id):
+        last_deposits = self.spot_client.deposit_history()['data']
+        for last_deposit in last_deposits:
+            amount_equal = float(last_deposit['amount']) == float(amount)
+            coin_equal = last_deposit['coin'] == 'BUSD'
+            addr_correct = last_deposit['address'] == self.address
+            tx_id_equal = last_deposit['txId'] == tx_id
+            transfer_successful = int(last_deposit['status']) == 1
 
-def check_deposit(amount: int, tx_id):
-    address = '0xad0625b44debf3a1b76acec973fb051ee8b2fa5f'
-    last_deposit = spot_client.deposit_history()['data'][0]
-    print(last_deposit)
-    if float(last_deposit['amount']) == float(amount) and last_deposit['coin'] == 'BUSD' and last_deposit[
-        'address'] == add0ress and int(last_deposit['status']) == 1 and last_deposit['txId'] == tx_id:
-        return True
-    else:
+            if amount_equal and coin_equal and addr_correct and tx_id_equal and transfer_successful:
+                return True
+
         return False
