370140d19ac06c726444cc48891f2459df69f41f
maxx4454 <93541282+maxx4454@users.noreply.github.com>
2022-08-14 17:55:38 +0300

    Update market_making.py

--- src/market_making.py
+++ src/market_making.py
@@ -1,54 +1,31 @@
-# takes item string as input, returns best sell order: tuple (id, amount) for given item from confirmed offers
-def find_best_sell_offer(item: str):
-    conn = ()
-    c = conn.cursor()
-    # c_market.execute('SELECT user_id, side, amount, item FROM order_data WHERE Id = (SELECT MIN(Id) FROM order_data)')
-    c.execute(
-        f"SELECT id, amount FROM verified_data WHERE item = '{item}' AND amount = (SELECT MIN(amount) FROM verified_data WHERE side = 'SELL') AND side = 'SELL'")
-    sell_offers = c.fetchall()
-    return sell_offers[0]
+from db import Database
+db = Database()
 
 
-# takes item string as input, returns best buy order: tuple (id, amount) for given item from confirmed offers
-def find_best_buy_offer(item: str):
-    conn = get_connection_verified()
-    c = conn.cursor()
-    # c_market.execute('SELECT user_id, side, amount, item FROM order_data WHERE Id = (SELECT MIN(Id) FROM order_data)')
-    c.execute(
-        f"SELECT id, amount FROM verified_data WHERE item = '{item}' AND amount = (SELECT MAX(amount) FROM verified_data WHERE side = 'BUY') AND side = 'BUY'")
-    buy_offers = c.fetchall()
-    return buy_offers[0]
+class MarketMaking():
+    # gets best sell and buy offer for a given item and commits a trade when SELL_PRICE <= BUY_PRICE
+    def conduct_trade(self, item: str):
+        sell_offer = db.find_best_sell_offer(item)
+        buy_offer = db.find_best_buy_offer(item)
 
+        id_sell = sell_offer[0]
+        id_buy = buy_offer()[0]
+        price_sell = sell_offer[1]
+        price_buy = buy_offer[1]
 
-# gets best sell and buy offer for a given item and commits a trade when SELL_PRICE <= BUY_PRICE
-def conduct_trade(item: str):
-    conn = get_connection_verified()
-    c = conn.cursor()
-    sell_offer = find_best_sell_offer(item)
-    buy_offer = find_best_buy_offer(item)
+        if price_buy >= price_sell:
+            self.trade(id_sell, id_buy)
 
-    id_sell = sell_offer[0]
-    id_buy = buy_offer()[0]
-    price_sell = sell_offer[1]
-    price_buy = buy_offer[1]
+            db.remove_id(id_sell)
+            db.remove_id(id_sell)
 
-    if price_buy >= price_sell:
-        trade(id_sell, id_buy)
+    # TODO: should exchange cash and product between participants of the deal
+    def trade(self, id_sell, id_buy):
+        print('exchanged')
 
-        c.execute(f"DELETE FROM verified_data WHERE Id = '{id_sell}'")
-        c.execute(f"DELETE FROM verified_data WHERE Id = '{id_buy}'")
-
-        c.commit()
-
-
-# TODO: should exchange cash and product between participants of the deal
-def trade(id_sell, id_buy):
-    print('exchanged')
-
-
-# runs continuously
-# loops through all traded items and conducts trades if they are available
-def market_making():
-    while True:
-        for item in read_items():
-            conduct_trade(item)
+    # runs continuously
+    # loops through all traded items and conducts trades if they are available
+    def market_making(self):
+        while True:
+            for item in db.read_items():
+                self.conduct_trade(item)
