cc5e969f209460b293e4ad047c46370a62a2baba
maxx4454 <93541282+maxx4454@users.noreply.github.com>
2022-08-15 15:49:52 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,26 +8,33 @@ class Utils:
     # check if wallet address is ok else return None
     @staticmethod
     def input_address(msg):
-        flag = True
         if len(msg) != 42:
-            flag = False
+            return False
+        if msg[:2] != '0x':
+            return False
         for c in msg:
             if not c.isalpha() or not c.isdigit():
-                flag = False
-                break
-        if flag:
-            return msg
-        else:
-            return None
+                return False
+        return True
 
     @classmethod
     def print_best_offers(cls, best_offers):
         (best_buy_orders, best_sell_orders) = best_offers
-        s = 'best offers: ' + "\n"
+        s = ""
         for item in best_buy_orders:
             s += item
 
         for item in best_sell_orders:
             s += item
 
+        return s
+
+    @classmethod
+    def generate_creds_string(cls, creds, deal_amount):
+        creds_left = creds[deal_amount - 1:]
+
+        s = ""
+        for cred in creds_left:
+            s += cred
+            s += "\n"
         return s
