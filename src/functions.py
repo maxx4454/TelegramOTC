7cf9dd05c9553aeaaa14281d0c6d6981fae8551c
Nik0Bellic <66374117+Nik0Bellic@users.noreply.github.com>
2022-08-16 17:48:04 +0300

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -8,6 +8,7 @@ from market_making import *
 
 bot = telebot.TeleBot(TOKEN)
 bt = Buttons()
+ut = Utils()
 
 db.init()
 
@@ -18,13 +19,22 @@ class Order:
 
     # Обрабатывает нажатие на кнопку мои ордера
     def get_my_orders(self, user_id):
-        s = ''
+        buy_orders = ''
+        sell_orders = ''
         orders = db.find_active_orders(user_id)
-        for i in range(len(orders)):
-            s += f'{i + 1} - {str(orders[i])}\n'
-        bot.send_message(user_id, s)
-        bot.send_message(user_id, 'Напиши номер ордера который хочешь изменить или 0 чтобы вернуться',
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
+        if len(orders) != 0:
+            for i in range(len(orders)):
+                if orders[i][1] == 'buy':
+                    buy_orders += f'{i + 1} - Тип_товар: {orders[i][3]}, Количество: {str(orders[i][2])}, Цена: {str(orders[i][4])}\n'
+                else:
+                    sell_orders += f'{i + 1} - Тип_товар: {orders[i][3]}, Количество: {str(orders[i][2])}, Цена: {str(orders[i][4])}\n'
+            bot.send_message(user_id, '<b>Твои ордера на покупку:</b>\n\n' + buy_orders, parse_mode='html')
+            bot.send_message(user_id, '<b>Твои ордера на продажу:</b>\n\n' + sell_orders, parse_mode='html')
+            bot.send_message(user_id, 'Напиши номер ордера (число слева) который хочешь изменить или 0 чтобы вернуться',
+                             reply_markup=telebot.types.ReplyKeyboardRemove())
+        else:
+            bot.send_message(user_id, 'У тебя нет активных ордеров')
+            bot.send_message(user_id, 'Главное меню', reply_markup=main)
 
     def manage(self, user_id, msg):
         orders = db.find_active_orders(user_id)
@@ -32,7 +42,7 @@ class Order:
             bot.send_message(user_id, 'Главное меню', reply_markup=main)
         else:
             order_id = int(msg) - 1
-            bot.send_message(user_id, str(orders[order_id]))
+            bot.send_message(user_id, f'{order_id} - Тип_товар: {orders[order_id][3]}, Количество: {str(orders[order_id][2])}, Цена: {str(orders[order_id][4])}\n')
             bot.send_message(user_id, 'Что ты хочешь изменить в этом ордере?', reply_markup=buttons_manage)
 
     def change_order(self, user_id, msg):
@@ -70,7 +80,7 @@ class Order:
 
     def get_item(self, user_id, msg):
         self._order['item'] = self._order['type'] + '_' + msg
-        best_offers_string = Utils.print_best_offers(db.get_best_offers(msg))
+        best_offers_string = ut.print_best_offers(db.get_best_offers(msg))
         if db.find_item(self._order['item']):
             bot.send_message(user_id, 'best offers: ' + "\n" + best_offers_string)
             bot.send_message(user_id, 'Сколько штук?')
@@ -80,11 +90,11 @@ class Order:
             return False
 
     def get_amount(self, user_id, msg):
-        self._order['amount'] = Utils.input_int(msg)
+        self._order['amount'] = ut.input_int(msg)
         bot.send_message(user_id, 'Сколько стоит?')
 
     def get_price(self, user_id, msg):
-        self._order['price'] = Utils.input_int(msg)
+        self._order['price'] = ut.input_int(msg)
 
     def request_confirm_order(self, user_id):
         bot.send_message(user_id,
@@ -98,22 +108,22 @@ class Order:
 
     def check(self, user_id, msg):
         if msg == bt.yes:
-            bot.send_message(user_id,
-                             'Отлично! Добавим твой ордер в базу\ncredentials or Напиши в чат tx_id когда транзакция подтвердится',
-                             reply_markup=telebot.types.ReplyKeyboardRemove())
-            return True
+            if self._order['side'] == 'buy':
+                bot.send_message(user_id,
+                                 'Отлично! Добавим твой ордер в базу\nНапиши в чат tx_id когда транзакция подтвердится',
+                                 reply_markup=telebot.types.ReplyKeyboardRemove())
+                return 'buy'
+            else:
+                bot.send_message(user_id,
+                                 'Отлично! Добавим твой ордер в базу\nОтправь файл формата .txt со всеми credentials по указанным товарам, каждый товар на отдельной строке',
+                                 reply_markup=telebot.types.ReplyKeyboardRemove())
+                return 'sell'
 
         if msg == bt.no:
             bot.send_message(user_id, 'Давай разбираться что не так',
                              reply_markup=telebot
                              .types.ReplyKeyboardRemove())
-            return False
 
-    def confirm_order(self, user_id, msg):
-        if self._order['side'] == 'buy':
-            self.confirm_payment(user_id, msg)
-        else:
-            self.confirm_credentials(user_id, msg)
 
     def confirm_payment(self, user_id, tx_id):
         payment = int(self._order['price']) * int(self._order['amount'])
@@ -122,6 +132,18 @@ class Order:
             bot.send_message(user_id, 'ордер выставлен', reply_markup=main)
         else:
             bot.send_message(user_id, 'что-то не так. пиши админу')
+
+    def get_credentials(self, user_id, msg):
+        file_info = bot.get_file(msg.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        src = '../resources/credentials/' + msg.document.file_name
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        with open(src, 'r') as f:
+            credentials = f.read()
+        # credentials - строка с содержимым файла
+        self.confirm_credentials(user_id, credentials)
 
     def confirm_credentials(self, user_id, msg):
         self._order['credentials'] = msg
@@ -140,11 +162,15 @@ class Order:
     def admin(self, user_id):
         s = ''
         orders = db.find_unverified()
-        for i in range(len(orders)):
-            s += f'{i + 1} - {str(orders[i])}\n'
-        bot.send_message(user_id, s)
-        bot.send_message(user_id, 'Напиши номер ордера который хочешь изменить или 0 чтобы вернуться',
-                         reply_markup=telebot.types.ReplyKeyboardRemove())
+        if len(orders) != 0:
+            for i in range(len(orders)):
+                s += f'{i + 1} - {str(orders[i])}\n'
+            bot.send_message(user_id, s)
+            bot.send_message(user_id, 'Напиши номер ордера который хочешь изменить или 0 чтобы вернуться',
+                             reply_markup=telebot.types.ReplyKeyboardRemove())
+        else:
+            bot.send_message(user_id, 'Нет необработанных ордеров')
+            bot.send_message(user_id, 'Главное меню', reply_markup=main)
 
     def admin_manage(self, user_id, msg):
         orders = db.find_unverified()
@@ -180,7 +206,7 @@ class Order:
         # new_credentials - строка с содержимым файла
         bot.send_message(user_id, 'Главное меню', reply_markup=main)
 
-    # Обрабатывает нажатие на кнопу мой адресс
+    # Обрабатывает нажатие на кнопку мой адресс
     def get_my_address(self, user_id):
         if db.return_address(user_id):
             bot.send_message(user_id, f'Твой адресс: {db.return_address(user_id)}')
@@ -191,7 +217,7 @@ class Order:
     # Обрабатывает нажатие на кнопку редактировать адресс
     def edit_address(self, user_id):
         if db.return_address(user_id):
-            bot.send_message(user_id, f'Твой текущий адресс: {db.return_address(user_id)}')
+            bot.send_message(user_id, f'Твой текущий адресс: {db.return_address(user_id)[0]}')
             bot.send_message(user_id, 'Введи новый адрес сети bep20:')
         else:
             bot.send_message(user_id, 'Введи свой адрес сети bep20:',
@@ -199,9 +225,9 @@ class Order:
 
     def get_new_address(self, user_id, msg):
         db.remove_address(user_id)
-        if Utils.input_address(msg):
+        if ut.input_address(msg):
             db.add_address(user_id, msg)
+            bot.send_message(user_id, 'Твой адресс изменен', reply_markup=main)
         else:
-            bot.send_message(user_id, 'wrong format try again', reply_markup=main)
+            bot.send_message(user_id, 'Неверный формат', reply_markup=main)
 
-        bot.send_message(user_id, 'Твой адресс изменен', reply_markup=main)
