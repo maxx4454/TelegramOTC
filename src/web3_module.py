21d6fb825bbf25d5489e93c89ca9067b83ad28f9
Nik0Bellic <66374117+Nik0Bellic@users.noreply.github.com>
2022-08-17 21:02:35 +0300

    Update web3_module.py

--- src/web3_module.py
+++ src/web3_module.py
@@ -2,6 +2,7 @@ import logging
 
 import web3.exceptions
 from web3 import Web3, HTTPProvider
+from resources.config import *
 
 
 class Bsc:
@@ -12,7 +13,7 @@ class Bsc:
     @classmethod
     def withdraw(cls, amount: int, address):
         try:
-            usdc = cls.w3.toChecksumAddress("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d")
+            usdc = cls.w3.toChecksumAddress(ADM_WALLET)
             abi = [{"inputs":[{"internalType":"address","name":"logic","type":"address"},{"internalType":"address","name":"admin","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":False,"inputs":[{"indexed":False,"internalType":"address","name":"previousAdmin","type":"address"},{"indexed":False,"internalType":"address","name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":False,"inputs":[{"indexed":True,"internalType":"address","name":"implementation","type":"address"}],"name":"Upgraded","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"implementation","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newImplementation","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"stateMutability":"payable","type":"function"},{"stateMutability":"payable","type":"receive"}]
             usdc_contract = cls.w3.eth.contract(usdc, abi=abi)
             nonce = cls.w3.eth.getTransactionCount(cls.public)
@@ -38,7 +39,7 @@ class Bsc:
     def check_deposit(cls, amount: int, tx_id):
         try:
             tx = cls.w3.eth.get_transaction(tx_id)
-            usdc = cls.w3.toChecksumAddress("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d")
+            usdc = cls.w3.toChecksumAddress(ADM_WALLET)
             amount *= 10 ** 18
             if tx['to'] == usdc:
                 # print('found somme')
