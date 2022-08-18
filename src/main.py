0d734f3dea64dcfc0dd29448ccad3869e417442c
Nik0Bellic <66374117+Nik0Bellic@users.noreply.github.com>
2022-08-18 15:12:12 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -29,11 +29,43 @@ def get_credentials(message):
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
-                     'Привет! <b>Это бот для OTC.</b>\n\n'
-                     'Здесь ты можешь безопасно покупать и продавать товары со вторички\n'
-                     'Выбери действие, которое тебя интересует',
-                     parse_mode='html', reply_markup=main
+                     'Привет! <b>Это бот для OTC.</b>\n'
+                     'Здесь ты можешь безопасно покупать и продавать товары со вторички\n\n',
+                     parse_mode='html'
                      )
+    bot.send_message(message.chat.id,
+                     'Для того чтобы купить/продать что-то, <b>тебе понадобится:</b>\n'
+                     '1) Выбрать тип товара из предложенных\n'
+                     '2) Ввести название\n'
+                     '3) Ввести желаемое количество\n'
+                     '4) А также цену по которой ты хочешь купить/продать товар\n'
+                     '5) Удостовериться что все введено верно', parse_mode='html'
+                     )
+    bot.send_message(message.chat.id,
+                     'Далее, в зависимости от того какого действие ты хочешь выполнить, тебя попросят следующее:\n\n'
+                     'Если ты хочешь <b>купить</b>, то тебе будет необходимо:\n'
+                     '1) Отправить указанную сумму (цена * количество) на наш адрес (таже будет указан)\n'
+                     '2) Написать id транзакции, что мы могли ее найти\n'
+                     'После такого как админ найдет твою транзакцию, твой ордер будет выставлен, и виден другим пользователям\n\n'
+                     'Если ты хочешь <b>продить</b>, то тебе будет необходимо:\n'
+                     '1) Отправить файл .txt с данными от аккаунтов которые ты продаешь (в указанном количестве, один акк - одна строка в файле)\n'
+                     'После такого как админ верифицирует данные от аккаунта, твой ордер будет выставлен, и виден другим пользователям', parse_mode='html'
+                     )
+    bot.send_message(message.chat.id,
+                     'Еще пара слов о <b>структуре бота:</b>\n'
+                     'После того админ верифицирует ордера, пользователи смогут их видеть\n'
+                     'А как только найдутся два ордера купить - продать с одним товаром и ценой, они автоматически исполнятся, т.е.\n'
+                     '<i>Покупателю</i> - пришлем товар\n'
+                     '<i>Продавцу</i> - деньги на указанный адрес\n\n'
+                     'До этого момента, деньги будут хранится на нашем счете, а верифицированные данные на нашем защищенном сервере\n'
+                     'Таким образом, мы гарантируем надежность', parse_mode='html'
+                     )
+    bot.send_message(message.chat.id,
+                     'На этом инструктаж закончен, пользуйся :)\n\n'
+                     '<u>P.S. вводи данные корректо, где просят числа, вводи числа, где слова - там слова\n'
+                     'Это в твоих же интересах, чтобы обрабатывать и выставлять твои ордера как можно быстрее)</u>', parse_mode='html'
+                     )
+    bot.send_message(message.chat.id, 'Выбери действие из предложенных:', reply_markup=main)
     bot.register_next_step_handler(message, basic)
 
 
@@ -41,12 +73,24 @@ def start(message):
 def basic(message):
     # если нажал купить
     if message.text == bt.buy:
-        order.buy(message.chat.id)
-        bot.register_next_step_handler(message, get_type)
+        if db.return_address(message.chat.id):
+            order.buy(message.chat.id)
+            bot.register_next_step_handler(message, get_type)
+        else:
+            bot.send_message(message.chat.id,
+                             'Ты еще не ввел свой адрес. Пожалуйста нажми на кнопку редактировать адрес',
+                             reply_markup=main)
+            bot.register_next_step_handler(message, basic)
     # если нажал продать
     elif message.text == bt.sell:
-        order.sell(message.chat.id)
-        bot.register_next_step_handler(message, get_type)
+        if db.return_address(message.chat.id):
+            order.sell(message.chat.id)
+            bot.register_next_step_handler(message, get_type)
+        else:
+            bot.send_message(message.chat.id,
+                             'Ты еще не ввел свой адрес. Пожалуйста нажми на кнопку редактировать адрес',
+                             reply_markup=main)
+            bot.register_next_step_handler(message, basic)
     # если нажал мои ордера
     elif message.text == bt.my_orders:
         order.get_my_orders(message.chat.id)
