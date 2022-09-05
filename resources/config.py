b37854bf9e05efe8f5cf7ffa335f0d621042c078
Nik0Bellic <66374117+Nik0Bellic@users.noreply.github.com>
2022-09-05 15:00:33 +0300

    Update config.py

--- resources/config.py
+++ resources/config.py
@@ -1,4 +1,6 @@
-TOKEN = '5401299074:AAGT65lu93Vqk2JMfMLxhKM9mVni5fCxok8'
-DATA = '/Users/timuragliev/Desktop/Python/Farm/database.db'
+TOKEN = '5404369551:AAGXDshe2IWUBo0ikE0867Ei6Ugg6qo58qQ'
+USER_DATA = '../resources/user_data.db'
+CREDENTIALS = '../resources/credentials/'
+NEW_CREDENTIALS = '../resources/new_credentials/'
 ADMIN = ['892338763', '5406497291']
 ADM_WALLET = '0xeBf029D90D2b0396cF1d12275bbB51E34D3F6DCB'
